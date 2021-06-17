f1fc3168218b1a008df08ca014f6f4481a34b78f
Oke Edafe <greatedafeoke@gmail.com>
2021-06-17 01:39:26 -0700

    Modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
 {
 	char string[52];
-	strcpy(string,"Programming is like building a multilingual puzzle\n");
+
+	strcpy(string, "Programming is like building a multilingual puzzle\n");
 	puts(string);
-	return 0;
+	return (0);
 }
 
