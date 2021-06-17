b1e39bf0a136d18a8aa72c0c2c85243268a7dbba
Oke Edafe <greatedafeoke@gmail.com>
2021-06-17 01:26:23 -0700

    Added 3-name and 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char string[52];
+	strcpy(string,"Programming is like building a multilingual puzzle\n");
+	puts(string);
+	return 0;
+}
+
