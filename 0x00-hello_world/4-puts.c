3a7e8e330ac4b80deb7d044678873fc00ce028c1
Oke Edafe <greatedafeoke@gmail.com>
2021-06-17 03:47:36 -0700

    Corrected 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char string[52];
 
-	strcpy(string, "\"Programming is like building a multilingual puzzle\n");
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
 	puts(string);
 	return (0);
 }
