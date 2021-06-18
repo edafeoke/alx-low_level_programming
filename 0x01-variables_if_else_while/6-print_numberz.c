66339e0b08359109eeda60dbc401d88d4a62699a
Oke Edafe <greatedafeoke@gmail.com>
2021-06-17 23:55:16 -0700

    Added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ten = 10;
+	int count = 0;
+	int char_0 = 48;
+
+	while (count < ten)
+	{
+		putchar(count + char_0);
+		++count;
+	}
+	putchar('\n');
+	return (0);
+}
