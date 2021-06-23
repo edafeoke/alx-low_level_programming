b5e391c12b3ff896eafe1d9a8f51c43cd72d4571
Oke Edafe <greatedafeoke@gmail.com>
2021-06-23 13:51:41 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Print every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	char h0, h1, m0, m1 = 0;
+
+	while (m0 < 10)
+	{
+		_putchar(h1 + 48);
+		_putchar(h0 + 48);
+		_putchar(':');
+		_putchar(m1 + 48);
+		_putchar(m0 + 48);
+		_putchar('\n');
+		m0++;
+		if (m0 > 9)
+		{
+			m0 = 0;
+			m1++;
+		}
+		if (m1 > 5)
+		{
+			m1 = 0;
+			h0++;
+		}
+		if (h0 > 9)
+		{
+			h0 = 0;
+			h1++;
+		}
+		if (h1 > 1 && h0 > 3)
+			return;
+	}
+	return;
+}
+
