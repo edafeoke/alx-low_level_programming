d3fd8690143889be1a206a10257d09c5441add7c
Oke Edafe <greatedafeoke@gmail.com>
2021-06-23 04:20:36 -0700

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Adds to number
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: Result of the addition
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
