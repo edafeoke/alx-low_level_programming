9c81f9c65fe0aa22acee25f43e1524adf98d7240
Oke Edafe <greatedafeoke@gmail.com>
2021-06-23 05:13:46 -0700

    Added 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,23 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i > 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	else
