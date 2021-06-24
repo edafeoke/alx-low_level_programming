a82f1f37e6546af516069c75d754d90053b72b05
Oke Edafe <greatedafeoke@gmail.com>
2021-06-24 00:25:48 -0700

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiply two digits
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: Result (a*b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
