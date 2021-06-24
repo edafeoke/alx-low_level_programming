1cf3b574c4df82d236e3116653950e489f5baff6
Oke Edafe <greatedafeoke@gmail.com>
2021-06-24 05:25:13 -0700

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Prints numbers 0 - 9 without 2 & 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 || i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
