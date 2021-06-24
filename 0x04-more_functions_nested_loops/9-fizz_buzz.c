d8cdb8075fb517ee4637b54058140cc82ecdcc7d
Oke Edafe <greatedafeoke@gmail.com>
2021-06-24 12:37:07 -0700

    Added 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,23 @@ int main(void)
 	{
 		if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
+		if (n < 99)
+		{
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
