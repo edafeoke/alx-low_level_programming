80efc5e61d680e7b418629f5bd5998a14d8e9d6a
Oke Edafe <greatedafeoke@gmail.com>
2021-06-28 16:59:01 -0700

    Added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Prints string in even intervals
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
