2c5886449c411775c3bdb1309890daaae5917aa0
Oke Edafe <greatedafeoke@gmail.com>
2021-06-29 00:34:50 -0700

    Added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy string from src to dest
+ * @dest: pointer to destinaiton
+ * @src: pointer to source
+ *
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *pointer = dest;
+
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = 0;
+	return (ptr);
+}
+
