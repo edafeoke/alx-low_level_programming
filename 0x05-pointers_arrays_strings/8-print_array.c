724b8f31c494bac607b297e7e45e3bf5ff9c10d8
Oke Edafe <greatedafeoke@gmail.com>
2021-06-28 23:46:28 -0700

    Added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * @a: pointer to the array
+ * @n: Number of elements to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count < a[n])
+	{
+		printf("%d", a[n]);
+		if (count < n-1)
+		{
+			printf(", ");
+		count++;
+	}
+	_putchar('\n');
+}
+
