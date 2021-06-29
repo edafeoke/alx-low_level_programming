411142714fc16d4867376b8cca1f05ed369b25e7
Oke Edafe <greatedafeoke@gmail.com>
2021-06-28 17:18:04 -0700

    Added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * _strlen -  swaps the values of two integers.
+ * @s: String input
+ *
+ * Return: Lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * puts_half - Prints half of a string, followed by a new line.
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void puts_half(char *s)
+{
+	int lenght = _strlen(s);
+	int n;
+
+	if (lenght % 2 == 0)
+	{
+		n = lenght / 2;
+	}
+	else
+	{
+		n = (length_of_the_string - 1) / 2
+
+	while (s[n])
+	{
+		_putchar(s[n]);
+		n++;
+	}
+	_putchar('\n');
+}
+
