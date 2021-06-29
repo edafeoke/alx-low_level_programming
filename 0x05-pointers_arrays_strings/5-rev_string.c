b01eb4a74d1a33ba2a3144e3a0b8689b987808be
Oke Edafe <greatedafeoke@gmail.com>
2021-06-29 01:00:55 -0700

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _strlen -  swaps the values of two integers.
+ * @s: String input
+ *
+ * Return: Lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * rev_string - Printsca string in reverse
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i = _strlen(s);
+	int j = 0;
+
+	while (j < i / 2)
+	{
+		tmp = s[i];
+		s[j] = s[i - 1 - j];
+		s[i - 1 - j] = tmp;
+		j++;
+	}
+}
+
