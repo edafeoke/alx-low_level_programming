61293062e66f97166f74347660bc733340fd6fde
Oke Edafe <greatedafeoke@gmail.com>
2021-06-30 07:50:08 -0700

    Added 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination
+ * @src: Source
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to the destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest_ptr;
+	int count;
+
+	dest_ptr = dest;
+	for (count = 0; count < n && *src != '\0'; count++)
+	{
+		dest[count] = src[count];
+	}
+	for (; count < n; count++)
+	{
+		dest[count] = '\0';
+	}
+	return (dest_ptr);
+}
