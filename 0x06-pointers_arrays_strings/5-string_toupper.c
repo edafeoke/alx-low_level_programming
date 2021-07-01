c1c4ba18079a1c28ec0503155820faf444122a75
Oke Edafe <greatedafeoke@gmail.com>
2021-06-30 19:10:56 -0700

    Added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @c: input characters
+ *
+ * Return: Pointer to characters
+ */
+char *string_toupper(char *c)
+{
+	while (*c != '\0')
+	{
+		if (*c >= 97 && *c <= 122)
+		{
+			*c = *c - 32;
+		}
+		c++;
+	}
+	return (c);
+}
