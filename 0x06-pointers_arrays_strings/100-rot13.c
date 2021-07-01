5310b59f146565c5e08e904297d3f1de3ca08d7e
Oke Edafe <greatedafeoke@gmail.com>
2021-07-01 05:05:03 -0700

    Added 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rot13 - changes all letters of a string to leet.
+ * @c: input characters
+ *
+ * Return: Pointer to characters
+ */
+char *rot13(char *c)
+{
+	char *alphas = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *codes = "nopqrstuvwxyzabcdefghijklmnNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		int j = 0;
+
+		while (alphas[j] != '\0')
+		{
+			if (c[i] == alphas[j])
+			{
+				c[i] = codes[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (c);
+}
