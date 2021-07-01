b207ee03448ef9cd66c6295ee3330eb9770c3402
Oke Edafe <greatedafeoke@gmail.com>
2021-06-30 23:22:34 -0700

    Added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: Differencwe between s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
