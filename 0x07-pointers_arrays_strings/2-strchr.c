16c9f0775cee869fa8902769c6b3bf9810e68498
Oke Edafe <greatedafeoke@gmail.com>
2021-07-05 12:29:19 -0700

    Added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr - copies memory area.
+ * @s: string to search
+ * @c: char to search for
+ *
+ * Return: s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
