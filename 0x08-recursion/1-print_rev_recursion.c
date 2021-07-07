11f828003bcc4994cb964ca4fde28140a13b3ce6
Oke Edafe <greatedafeoke@gmail.com>
2021-07-07 06:35:22 -0700

    Added 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
+ * _print_rev_recursion - prints a string, followed by a new line.
  * @s: String
  *
  * Return: void;
