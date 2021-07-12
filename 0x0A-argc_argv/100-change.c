304d6dfb5eda0677241f200571adcd6b9bf0fa9d
Oke Edafe <greatedafeoke@gmail.com>
2021-07-12 16:57:21 -0700

    Added advance task 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: list of args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int cent[5] = {25, 10, 5, 2, 1};
+	int i = 0;
+	int count = 0;
+	int tmp = atoi(argv[1]);
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (tmp > 0)
+	{
+		if (tmp >= cent[i])
+		{
+			tmp = tmp - cent[i];
+			count++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
