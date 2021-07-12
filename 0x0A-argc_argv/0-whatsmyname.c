059a198d92981ba63ca1226b2b319f8a91cd1cda
Oke Edafe <greatedafeoke@gmail.com>
2021-07-12 00:45:51 -0700

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: list of args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
