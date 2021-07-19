fb95e2bdf0e9761dda13b74c2b8f630fc5697bae
Oke Edafe <greatedafeoke@gmail.com>
2021-07-19 11:09:17 -0700

    Added 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,10 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n") :
 			printf("Name: %s\n", (*d).name);
 		(*d).age ?
-			printf("Age: %f\n", (*d).age):
+			printf("Age: %f\n", (*d).age) :
 			printf("Age: (nil)\n");
 		(*d).owner ?
-			printf("Owner: %s\n", (*d).owner):
+			printf("Owner: %s\n", (*d).owner) :
 			printf("Owner: (nil)");
 	}
 }
