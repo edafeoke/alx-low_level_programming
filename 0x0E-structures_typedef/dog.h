5d44ea9d232ec91c2d62f4e262cd37125bd9be88
Oke Edafe <greatedafeoke@gmail.com>
2021-07-19 07:25:11 -0700

    Added dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG
 #define DOG
 
-struct dog {
+/**
+ * struct dog - Structure of dog
+ * @name: the name
+ * @age: the age
+ * @owner: name of the owner
+ *
+ * Description: Structure of a dog containing name , age
+ * and owner.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};
-
+}
 #endif
