ed0c39335ac03e1ccc183b2c87b1c80fdc5fd6e6
Oke Edafe <greatedafeoke@gmail.com>
2021-07-19 07:43:19 -0700

    Added 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize the dog struct
+ * @d: dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
