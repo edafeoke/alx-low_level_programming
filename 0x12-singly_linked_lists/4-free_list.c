c77381856a1e0b222edf0631f5dd773dc94ca56b
Oke Edafe <greatedafeoke@gmail.com>
2021-07-29 10:16:41 -0700

    Added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: first node of list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
