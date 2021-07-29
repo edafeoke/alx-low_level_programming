3f3b528ee5679a55236dadb3b8fd29665a84136f
Oke Edafe <greatedafeoke@gmail.com>
2021-07-29 10:50:27 -0700

    Modified 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		if (temp->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", temp->len, temp->str);
 		temp = temp->next;
 		count++;
 	}
