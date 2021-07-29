29a71f955a0cd4c6a4eddb7ebed2ee05da584c48
Oke Edafe <greatedafeoke@gmail.com>
2021-07-28 23:32:36 -0700

    Added 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints content of a linked list
+ * @h: head of list to be printed
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h -> next;
+		count++;
+	}
+	return (count);
+}
