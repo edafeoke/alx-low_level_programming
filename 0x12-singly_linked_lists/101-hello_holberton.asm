dbdc751313123d2e3f2f2818d7debedb10c416e3
Oke Edafe <greatedafeoke@gmail.com>
2021-07-29 11:26:21 -0700

    Added 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,9 @@
+          global    main
+          extern    puts
+
+          section   .text
+main:                                       ; This is called by the C library startup code
+          mov       rdi, message            ; First integer (or pointer) argument in rdi
+          call      puts                    ; puts(message)
+          ret                               ; Return from main back into C library wrapper
+message:  db        "Hello, Holberton", 0
