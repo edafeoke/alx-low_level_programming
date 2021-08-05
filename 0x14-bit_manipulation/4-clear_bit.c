d64de9ea611f83a0e0153551d05b3ee109267462
Oke Edafe <greatedafeoke@gmail.com>
2021-08-05 03:05:43 -0700

    Added 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - Clears value of a bit to 1
+ * @n: number
+ * @index: index
+ *
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index <= 64)
+	{
+		*n = *n & ~(1 << index);
+		return (1);
+	}
+	return (-1);
+}
