de5d166b17a6bbb201beb4e9b373ba51bc455023
Oke Edafe <greatedafeoke@gmail.com>
2021-08-09 02:36:11 -0700

    Updated holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
