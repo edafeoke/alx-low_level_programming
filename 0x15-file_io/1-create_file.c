38754d43281bc66ada7c475a4a6b2400844e58f5
Oke Edafe <greatedafeoke@gmail.com>
2021-08-10 05:27:35 -0700

    Fixed bug in 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (fd);
 	for (i = 0 ; text_content[i] != '\0' ; i++)
