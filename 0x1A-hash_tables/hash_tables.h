92c90d0c6bf5237ec5e7101a1bd80a981a0f4978
Oke Edafe Great <greatedafeoke@gmail.com>
2021-10-07 06:05:32 -0700

    Added 1-djb2.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,5 +35,8 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str);
+
 
 #endif /* HASH_TABLES  */
