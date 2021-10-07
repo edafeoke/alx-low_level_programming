1c9cc1f187e289a4793db7e28e5188ffcfe25235
Oke Edafe Great <greatedafeoke@gmail.com>
2021-10-07 06:39:52 -0700

    Added 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ * *key: key
+ * @value: value
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index, i = 0;
+	char *value_copy;
+	hash_node_t *new;
+
+
+	if (*key == '\0' || ht == NULL || key == NULL || value == NULL)
+		return (0);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		free(value_copy);
+	return (0);
+	new->key = strdup(key);
+	if (new->key == NULL)
+		free(new);
+	return (0);
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
