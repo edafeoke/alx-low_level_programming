63013798dfe68e72327b8d74f365cbadafaf9744
Oke Edafe Great <greatedafeoke@gmail.com>
2021-12-02 18:32:24 -0800

    Attempting task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,19 +3,22 @@
 size of an island module
 '''
 
+
 def island_perimeter(grid):
     '''
     returns the perimeter of the island described in grid
     '''
-    perimeter = 0 
-    
+    perimeter = 0
+
     for r in range(0, len(grid)):
         for c in range(0, len(grid[r])):
             if grid[r][c] == 1:
-                if grid[r - 1][c] == 0:
-                    perimeter += 1
-                if grid[r + 1][c] == 0:
-                    perimeter += 1
+                if r > 0:
+                    if grid[r - 1][c] == 0:
+                        perimeter += 1
+                if r < len(grid):
+                    if grid[r + 1][c] == 0:
+                        perimeter += 1
                 if grid[r][c - 1] == 0:
                     perimeter += 1
                 if grid[r][c + 1] == 0:
